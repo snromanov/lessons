4249850028914e6e20efa672c994770e4e4f895e
snromanov <41792342+snromanov@users.noreply.github.com>
2018-08-09 17:59:15 +0300

    промежуточное

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,14 +2,17 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-    double p1 = 4.0;
-    double p2 = 6.0;
-}
+    public double p1;
+    public double p2;
 
     public Point(double p1, double p2) {
         this.p1 = p1;
         this.p2 = p2;
 
+        public  double area() {
+            return this.p1 * this.p2;
+        }
+    }
 
 
 }
