678f6ee7e6dec5c50a87da00b37b54f1ecd578ed
snromanov <41792342+snromanov@users.noreply.github.com>
2018-08-15 13:38:07 +0300

    Задание 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -10,6 +10,14 @@ public class MyFirstProgram {
         Point p1 = new Point(2, 4);
         Point p2 = new Point(5, 8);
 
+
+        /*
+        //Используем  функцию
+        System.out.println("Расстояние между  двумя точками p1 и p2= " + Point.distance (p1,p2));
+        */
+
+
+        //Используем  метод
         System.out.println("Расстояние между  двумя точками p1 и p2 = " + p1.distance(p2));
     }
 
