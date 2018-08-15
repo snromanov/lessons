bf62c87f545353da1ce923668c3e3230f2fe20e6
snromanov <41792342+snromanov@users.noreply.github.com>
2018-08-15 10:09:39 +0300

    задание 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -15,7 +15,7 @@ public class Point {
     public static double distance(Point p1, Point p2) {
       double dx = p1.x - p2.x; // по формуле x2-x1
       double dy = p1.y - p2.y; // по формуле y2-y1
-      return Math.sqrt(dx * dx + dy * dy); //квадратный корень
+      return Math.sqrt(dx * dx + dy * dy); // выводим  квадратный корень
 
 
 
