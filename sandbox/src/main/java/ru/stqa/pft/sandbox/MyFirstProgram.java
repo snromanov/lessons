98a81842374e9ce36730c0ba5fe3d861cb83b6d1
snromanov <41792342+snromanov@users.noreply.github.com>
2018-08-15 13:25:16 +0300

    Задание 2  вторая попытка

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,14 +4,14 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 
-  public static void main (String[] args) //вводим аргументы
+    public static void main(String[] args) //вводим аргументы
 
-  {
-    Point p1 = new Point(2, 4);
-    Point p2 = new Point(5, 8);
+    {
+        Point p1 = new Point(2, 4);
+        Point p2 = new Point(5, 8);
 
-    System.out.println("Расстояние между  двумя точками p1 и p2= " + Point.distance (p1,p2));
-  }
+        System.out.println("Расстояние между  двумя точками p1 и p2 = " + p1.distance(p2));
+    }
 
 
-  }
+}
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,25 +1,22 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-  double x, y;
 
-  public Point(double x, double y) {
-    this.x = x;
-    this.y = y;
-  }
+    private final double x;
+    private final double y;
 
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
- 
-
-    //Вводим функцию
-    public static double distance(Point p1, Point p2) {
-      double dx = p1.x - p2.x; // по формуле x2-x1
-      double dy = p1.y - p2.y; // по формуле y2-y1
-      return Math.sqrt(dx * dx + dy * dy); // выводим  квадратный корень
+    public double distance(Point p2) {
+        double dx = this.x - p2.x; // по формуле x2-x1
+        double dy = this.y - p2.y; // по формуле y2-y1
+        return Math.sqrt(dx * dx + dy * dy); // выводим  квадратный корень
 
 
-
-  }
+    }
 
 }
 
