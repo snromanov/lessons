13353cc42fec2e3c1dae008e2fb4230e3397299c
snromanov <41792342+snromanov@users.noreply.github.com>
2018-08-17 11:17:21 +0300

    к уроку заданию 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -1,13 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Square {
-    private final double l;
+    public double l;
 
     public Square(double l) {
         this.l = l;
     }
-
     public double area() {
-        return this.l * this.l;
+
+        return this.l =  this.l;
+
+
     }
 }
