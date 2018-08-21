b7d39cca2b162c283c563f12a2608e93b84a71af
IStas <istas@Mac-mini.local>
2018-08-21 20:41:14 +0300

    newmodelClass
    1. Создан новый модельный  класс ContactData
    2. Завершение рефакторинга

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AbonentCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AbonentCreationTests.java
@@ -4,16 +4,11 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class AbonentCreationTests {
     FirefoxDriver wd;
@@ -23,50 +18,51 @@ public class AbonentCreationTests {
         wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/index.php");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).sendKeys("\\undefined");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
-    public void AbonentCreationTests() {
-        initAbonentCreation();
-        fillAbonentForm();
-        returnAbonentPage();
+    public void contactCreationTests() {
+        initContactCreation();
+        fillContactForm(new ContactData("Терьентий", "Терьентьтев", "89500000000", "ma@mail.ru"));
+        returnConcactPage();
     }
 
-    private void returnAbonentPage() {
+    private void returnConcactPage() {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillAbonentForm() {
+    private void fillContactForm(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Vasiliy");
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Pupkin");
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getSecondname());
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys("89500000000");
+        wd.findElement(By.name("home")).sendKeys(contactData.getTelnumber());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("ma@mail.ru");
+
+        wd.findElement(By.name("email")).sendKeys(contactData.getMail());
     }
 
 
-    private void initAbonentCreation() {
+    private void initContactCreation() {
         wd.findElement(By.linkText("add new")).click();
     }
 
