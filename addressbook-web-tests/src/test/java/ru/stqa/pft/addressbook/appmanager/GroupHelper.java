9df735bebe3621c31180c9c3086e2da7b2623949
IStas <istas@Mac-mini.local>
2018-08-26 17:56:13 +0300

    введение параметра

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -8,39 +8,39 @@ public class GroupHelper {
   private FirefoxDriver wd;
 
   public GroupHelper(FirefoxDriver wd) {
-    this.wd =wd;
+    this.wd = wd;
 
   }
 
   public void returnToGroupPage() {
-      wd.findElement(By.linkText("group page")).click();
+    wd.findElement(By.linkText("group page")).click();
   }
 
   public void submitGroupCreation() {
-      wd.findElement(By.name("submit")).click();
+    wd.findElement(By.name("submit")).click();
   }
 
   public void fillGroupForm(GroupData groupData) {
-      wd.findElement(By.name("group_name")).click();
-      wd.findElement(By.name("group_name")).clear();
-      wd.findElement(By.name("group_name")).sendKeys(groupData.getHeader());
-      wd.findElement(By.name("group_header")).click();
-      wd.findElement(By.name("group_header")).clear();
-      wd.findElement(By.name("group_header")).sendKeys(groupData.getBody());
-      wd.findElement(By.name("group_footer")).click();
-      wd.findElement(By.name("group_footer")).clear();
-      wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    type(By.name("group_name"), groupData.getHeader());
+    type(By.name("group_header"), groupData.getBody());
+    type(By.name("group_footer"), groupData.getFooter());
+  }
+
+  private void type(By locator, String text) {
+    wd.findElement(locator).click();
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
   }
 
   public void initGroupCreation() {
-      wd.findElement(By.name("new")).click();
+    wd.findElement(By.name("new")).click();
   }
 
   public void deleteGroup() {
-      wd.findElement(By.name("delete")).click();
+    wd.findElement(By.name("delete")).click();
   }
 
   public void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
+    wd.findElement(By.name("selected[]")).click();
   }
 }
