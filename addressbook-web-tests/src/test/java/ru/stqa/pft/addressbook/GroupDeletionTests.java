e0de64f5a6e131ad45bf3353937b7a9b3f035e39
IStas <istas@Mac-mini.local>
2018-08-26 15:05:59 +0300

    восстановление  + выделение базового метода

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+
+
+  @Test
+  public void testGroupDeletionTests() {
+    gotoGroupPage();
+    selectGroup();
+    deleteGroup();
+    returnToGroupPage();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    wd.quit();
+  }
+
+
+}
