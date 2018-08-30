42213d82ad663f008241fc644aec73ed1cb7e3a0
IStas <istas@Mac-mini.local>
2018-08-30 19:01:49 +0300

    конец 6 видео

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -1,48 +1,49 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelperBase {
 
-    protected WebDriver wd;
+  protected WebDriver wd;
 
-    public HelperBase(WebDriver wd) {
-        this.wd = wd;
-    }
+  public HelperBase(WebDriver wd) {
+    this.wd = wd;
+  }
 
-    protected void click(By locator) {
-        wd.findElement(locator).click();
-    }
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
 
-    protected void type(By locator, String text) {
-        click(locator);
-        if (text != null) {
-            wd.findElement(locator).clear();
-            wd.findElement(locator).sendKeys(text);
-        }
-    }
-
-    public boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    protected void typeone(By locator, String text) {
-        wd.findElement(locator).click();
+  protected void type(By locator, String text) {
+    click(locator);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (!text.equals(existingText)) {
         wd.findElement(locator).clear();
         wd.findElement(locator).sendKeys(text);
+      }
     }
+  }
 
-    public void initGroupModification() {
-        click(By.name("edit"));
+  public boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
+  }
+
+  protected void typeone(By locator, String text) {
+    wd.findElement(locator).click();
+    wd.findElement(locator).clear();
+    wd.findElement(locator).sendKeys(text);
+  }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
 
 }
