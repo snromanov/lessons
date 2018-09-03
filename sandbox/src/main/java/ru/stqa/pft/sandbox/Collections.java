ec311792e460e40ca2cbbc70e5cbe102489520c4
IStas <istas@Mac-mini.local>
2018-09-03 18:16:50 +0300

    конец  4.2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,8 +1,18 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
   public static void main(String[] args) {
-    
-  }
+    String[] langs = {"Java", "C#", "Python", "PHP"};
 
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+
+    for (String l : languages) { // размер массива  указать  нельзя
+      System.out.println("Я хочу выучить " + l);
+    }
+  }
 }
