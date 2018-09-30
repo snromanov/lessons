ceb1373e86d9a72e8aa485a0026cb220c5a36236
IStas <istas@Mac-mini.local>
2018-09-30 19:55:06 +0300

    конец 6.4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -10,16 +13,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+
   public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
-
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
-
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
+
+  private void save(List<GroupData> groups, File file) throws IOException {
     System.out.println(new File(".").getAbsolutePath());
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
@@ -28,12 +46,10 @@ public class GroupDataGenerator {
     writer.close();
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
-      groups.add(new GroupData().withHeader(String.format("test %s", i))
-              .withBody(String.format("header %s", i))
-              .withFooter(String.format("footer %s", i)));
+      groups.add(new GroupData().withHeader(String.format("test %s", i)).withBody(String.format("header %s", i)).withFooter(String.format("footer %s", i)));
     }
     return groups;
   }
