52b3be9660e309ac1cb674ed1a7a336c2cbabcbd
IStas <istas@Mac-mini.local>
2018-10-01 21:37:20 +0300

    Домашнее задание  13
    реализованы парметризованные тесты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,11 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,24 +22,49 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
 
-  @Test
-  public void testContactCreation() {
+
+
+  @DataProvider
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml ="";
+    String line = reader.readLine();
+    while (line != null) {
+      xml +=line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(GroupData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return  contacts.stream().map((g) -> new Object [] {g}).collect(Collectors.toList()).iterator();
+
+  }
+
+
+  @DataProvider
+  public Iterator<Object[]> validContactsXml() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsXml")
+  public void testContactCreation(ContactData contact) {
     app.goTo().contactListPage();
     Contacts before = app.contact().all();
     app.goTo().createContactPage();
-    File photo = new File ("src/test/resources/123.jpg");
-    ContactData contact  =  new ContactData()
-            .withFirstName("testNewFirstName")
-            .withLastName("testNewLastName")
-            .withMiddleName("79991088888")
-            .withEmail1("New@New.ru")
-            .withYear("1888")
-            .withGroup("test0")
-            .withPhoto(photo);
     app.contact().create(contact);
     app.goTo().contactListPage();
     Contacts after = app.contact().all();
-    Assert.assertEquals(app.group().count(), before.size()+1);
+    Assert.assertEquals(app.group().count(), before.size() + 1);
     app.goTo().contactListPage();
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
