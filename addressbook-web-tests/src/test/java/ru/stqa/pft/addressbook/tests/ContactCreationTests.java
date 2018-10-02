c28b786c3fd7c924960824ea51694b1d4e854df8
IStas <istas@Mac-mini.local>
2018-10-02 21:40:49 +0300

    конец видео 6.9
    запуск  Gradle через консоль

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -26,34 +26,35 @@ public class ContactCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validContactsFromXml() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
-    String xml ="";
-    String line = reader.readLine();
-    while (line != null) {
-      xml +=line;
-      line = reader.readLine();
-    }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(GroupData.class);
-    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-    return  contacts.stream().map((g) -> new Object [] {g}).collect(Collectors.toList()).iterator();
-
+   try
+           (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))){
+     String xml ="";
+     String line = reader.readLine();
+     while (line != null) {
+       xml +=line;
+       line = reader.readLine();
+     }
+     XStream xstream = new XStream();
+     xstream.processAnnotations(GroupData.class);
+     List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+     return  contacts.stream().map((g) -> new Object [] {g}).collect(Collectors.toList()).iterator();
+   }
   }
 
 
   @DataProvider
   public Iterator<Object[]> validContactsXml() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
-    String xml = "";
-    String line = reader.readLine();
-    while (line != null) {
-      xml += line;
-      line = reader.readLine();
-    }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(ContactData.class);
-    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+   try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) { String xml = "";
+     String line = reader.readLine();
+     while (line != null) {
+       xml += line;
+       line = reader.readLine();
+     }
+     XStream xstream = new XStream();
+     xstream.processAnnotations(ContactData.class);
+     List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+     return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();}
+
   }
 
   @Test(dataProvider = "validContactsXml")
